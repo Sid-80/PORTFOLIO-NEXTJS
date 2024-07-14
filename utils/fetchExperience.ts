106fed20aa2a40593dd3f234b271a9294812dad4
Sid-80 <siddharthbramhecha@gmail.com>
2024-07-14 23:36:41 +0530

    fix: base url

--- utils/fetchExperience.ts
+++ utils/fetchExperience.ts
@@ -1,7 +1,7 @@
 import { Experience } from "@/typing";
 
 export const fetchExps = async() =>{
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || window.location.href}/api/getExperience`);
     const data = await res.json();
     const exps:Experience[] = data.experience;
     return exps;
