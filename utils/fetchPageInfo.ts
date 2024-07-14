cb4252a16c2517a116daf34af463b6f2b6c91556
Sid-80 <siddharthbramhecha@gmail.com>
2024-07-14 23:41:26 +0530

    fix: base url

--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,7 +1,7 @@
 import { PageInfo } from "@/typing";
 
 export const fetchPageInfo = async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`);
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL || window.location.href}/api/getPageInfo`);
     const data = await res.json();
     const PageInfos:PageInfo = data.pageInfo;
     return PageInfos; 
